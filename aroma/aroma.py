c1d11c13cefddcd797ff9197e37c4d53e80e62dd
Eneko <enekouru@gmail.com>
2020-11-02 10:21:29 +0100

    Bug fixes

--- aroma/aroma.py
+++ aroma/aroma.py
@@ -4,21 +4,28 @@ from future import standard_library
 standard_library.install_aliases()
 from builtins import str
 import os
+import os.path as op
 import argparse
 import subprocess
 import shutil
 from . import utils as aromafunc
 
 
-def aroma_workflow(args):
+def aroma_workflow(inFeat, inFile, mc, melDir, affmat, warp, outDir, dim,
+                   denType, mask, TR, overwrite, generate_plots):
+
+    # Change to script directory
+    cwd = op.realpath(op.curdir)
+    scriptDir = op.dirname(op.abspath(__file__))
+    os.chdir(scriptDir)
+
     print('\n------------------------------- RUNNING ICA-AROMA ------------------------------- ')
     print('--------------- \'ICA-based Automatic Removal Of Motion Artifacts\' --------------- \n')
 
     # Define variables based on the type of input (i.e. Feat directory or specific input arguments), and check whether the specified files exist.
     cancel = False
 
-    if args.inFeat:
-        inFeat = args.inFeat
+    if inFeat:
 
         # Check whether the Feat directory exists
         if not os.path.isdir(inFeat):
@@ -27,10 +34,10 @@ def aroma_workflow(args):
             exit()
 
         # Define the variables which should be located in the Feat directory
-        inFile = os.path.join(args.inFeat, 'filtered_func_data.nii.gz')
-        mc = os.path.join(args.inFeat, 'mc', 'prefiltered_func_data_mcf.par')
-        affmat = os.path.join(args.inFeat, 'reg', 'example_func2highres.mat')
-        warp = os.path.join(args.inFeat, 'reg', 'highres2standard_warp.nii.gz')
+        inFile = os.path.join(inFeat, 'filtered_func_data.nii.gz')
+        mc = os.path.join(inFeat, 'mc', 'prefiltered_func_data_mcf.par')
+        affmat = os.path.join(inFeat, 'reg', 'example_func2highres.mat')
+        warp = os.path.join(inFeat, 'reg', 'highres2standard_warp.nii.gz')
 
         # Check whether these files actually exist
         if not os.path.isfile(inFile):
@@ -47,16 +54,16 @@ def aroma_workflow(args):
             cancel = True
 
         # Check whether a melodic.ica directory exists
-        if os.path.isdir(os.path.join(args.inFeat, 'filtered_func_data.ica')):
-            melDir = os.path.join(args.inFeat, 'filtered_func_data.ica')
+        if os.path.isdir(os.path.join(inFeat, 'filtered_func_data.ica')):
+            melDir = os.path.join(inFeat, 'filtered_func_data.ica')
         else:
-            melDir = args.melDir
+            melDir = melDir
     else:
-        inFile = args.inFile
-        mc = args.mc
-        affmat = args.affmat
-        warp = args.warp
-        melDir = args.melDir
+        inFile = inFile
+        mc = mc
+        affmat = affmat
+        warp = warp
+        melDir = melDir
 
         # Check whether the files exist
         if not inFile:
@@ -81,13 +88,13 @@ def aroma_workflow(args):
                 cancel = True
 
     # Parse the arguments which do not depend on whether a Feat directory has been specified
-    outDir = args.outDir
-    dim = args.dim
-    denType = args.denType
+    outDir = outDir
+    dim = dim
+    denType = denType
 
     # Check if the mask exists, when specified.
-    if args.mask:
-        if not os.path.isfile(args.mask):
+    if mask:
+        if not os.path.isfile(mask):
             print('The specified mask does not exist.')
             cancel = True
 
@@ -107,12 +114,12 @@ def aroma_workflow(args):
     fslDir = os.path.join(os.environ["FSLDIR"], 'bin', '')
 
     # Create output directory if needed
-    if os.path.isdir(outDir) and args.overwrite is False:
+    if os.path.isdir(outDir) and overwrite is False:
         print('Output directory', outDir, """already exists.
             AROMA will not continue.
             Rerun with the -overwrite option to explicitly overwrite existing output.""")
         exit()
-    elif os.path.isdir(outDir) and args.overwrite is True:
+    elif os.path.isdir(outDir) and overwrite is True:
         print('Warning! Output directory', outDir, 'exists and will be overwritten.\n')
         shutil.rmtree(outDir)
         os.makedirs(outDir)
@@ -120,8 +127,8 @@ def aroma_workflow(args):
         os.makedirs(outDir)
 
     # Get TR of the fMRI data, if not specified
-    if args.TR:
-        TR = args.TR
+    if TR:
+        TR = TR
     else:
         cmd = ' '.join([os.path.join(fslDir, 'fslinfo'),
                         inFile,
@@ -136,12 +143,12 @@ def aroma_workflow(args):
         exit()
 
     # Define/create mask. Either by making a copy of the specified mask, or by creating a new one.
-    mask = os.path.join(outDir, 'mask.nii.gz')
-    if args.mask:
-        shutil.copyfile(args.mask, mask)
+    new_mask = os.path.join(outDir, 'mask.nii.gz')
+    if mask:
+        shutil.copyfile(mask, new_mask)
     else:
         # If a Feat directory is specified, and an example_func is present use example_func to create a mask
-        if args.inFeat and os.path.isfile(os.path.join(inFeat, 'example_func.nii.gz')):
+        if inFeat and os.path.isfile(os.path.join(inFeat, 'example_func.nii.gz')):
             os.system(' '.join([os.path.join(fslDir, 'bet'),
                                 os.path.join(inFeat, 'example_func.nii.gz'),
                                 os.path.join(outDir, 'bet'),
@@ -152,7 +159,7 @@ def aroma_workflow(args):
             if os.path.isfile(os.path.join(outDir, 'bet.nii.gz')):
                 os.remove(os.path.join(outDir, 'bet.nii.gz'))
         else:
-            if args.inFeat:
+            if inFeat:
                 print(' - No example_func was found in the Feat directory. A mask will be created including all voxels with varying intensity over time in the fMRI data. Please check!\n')
             os.system(' '.join([os.path.join(fslDir, 'fslmaths'),
                                 inFile,
@@ -163,7 +170,7 @@ def aroma_workflow(args):
     #---------------------------------------- Run ICA-AROMA ----------------------------------------#
 
     print('Step 1) MELODIC')
-    aromafunc.runICA(fslDir, inFile, outDir, melDir, mask, dim, TR)
+    aromafunc.runICA(fslDir, inFile, outDir, melDir, new_mask, dim, TR)
 
     print('Step 2) Automatic classification of the components')
     print('  - registering the spatial maps to MNI')
@@ -185,7 +192,7 @@ def aroma_workflow(args):
     print('  - classification')
     motionICs = aromafunc.classification(outDir, maxRPcorr, edgeFract, HFC, csfFract)
 
-    if args.generate_plots:
+    if generate_plots:
         from classification_plots import classification_plot
         classification_plot(os.path.join(outDir, 'classification_overview.txt'),
                             outDir)
