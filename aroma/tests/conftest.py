71300d5eee2564894024f9515beec5b28b3fc7ea
Taylor Salo <tsalo006@fiu.edu>
2020-11-04 10:38:31 -0500

    Run black on test files.

--- aroma/tests/conftest.py
+++ aroma/tests/conftest.py
@@ -3,17 +3,22 @@ import pytest
 
 from nilearn.datasets import fetch_development_fmri
 
+
 def pytest_addoption(parser):
-    parser.addoption('--skipintegration', action='store_true',
-                     default=False, help='Skip integration tests.')
+    parser.addoption(
+        "--skipintegration",
+        action="store_true",
+        default=False,
+        help="Skip integration tests.",
+    )
 
 
 @pytest.fixture
 def skip_integration(request):
-    return request.config.getoption('--skipintegration')
+    return request.config.getoption("--skipintegration")
 
 
-@pytest.fixture(scope='session')
+@pytest.fixture(scope="session")
 def testpath(tmp_path_factory):
     """ Test path that will be used to download all files """
     return tmp_path_factory.getbasetemp()
